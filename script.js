bcf0f8836270bbd5e5b65af7b80c571e5b56f365
Rianne <rianne.visser@outlook.com>
2023-01-04 21:10:20 +0100

    Add button to clear grid

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
+const backgroundColor = "#121111";
+
+
+
 let slider = document.getElementById("sizeRange");
 let output = document.getElementById("sizeIndicator");
 output.innerHTML = `${slider.value} x ${slider.value}`;
 
+let dimension = slider.value;
+let clearbutton = document.querySelector(".clearBtn");
+clearbutton.addEventListener("click", reload);
+
+slider.oninput = function() {
+    output.innerHTML = `${this.value} x ${this.value}`;
+}
+
+
+console.log(dimension);
+let container = document.querySelector(".sketchContainer");
 
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
 
-//Function asks for the dimension and creates the amount of divs inside the container based on what's returned in the prompt
-function createSketchPad () {
-    slider.oninput = function() {
-        output.innerHTML = `${this.value} x ${this.value}`;
-    }
-    let dimension = slider.value;
-    let container = document.querySelector(".sketchContainer");
+
+//Function creates the specified amount of divs inside the container
+function createSketchPad (dimension) {
     for (let i=0; i<dimension*dimension; i++) {
         let pad = document.createElement("div");
         let width = (1/dimension)*100;
@@ -27,11 +38,24 @@ function createSketchPad () {
     }    
 }
 
-createSketchPad();
-
 function changeColor(e) {
     if (e.type === 'mouseover' && !mouseDown) return
     if (e.type === 'mouseover' && mouseDown) {
         e.target.style.backgroundColor = "white";
     }
 }
+
+function reload () {
+    clearGrid()
+    createSketchPad(dimension);
+}
+
+function clearGrid () {
+    let pad = document.getElementsByClassName("pad");
+    while (pad.length>0) {
+        let singlepad = pad[0];
+        container.removeChild(singlepad);
+    } 
+}
+
+window.onload = createSketchPad(dimension);
